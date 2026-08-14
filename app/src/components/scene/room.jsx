5726a9e3b10600cf15e84fe88035267721fca385
Yanis Tessier <yanis.tessier@epitech.eu>
2026-08-14 07:51:05 +0200

    update: import 3D model in room component

--- app/src/components/scene/room.jsx
+++ app/src/components/scene/room.jsx
@@ -1,7 +1,10 @@
+import { useGLTF } from '@react-three/drei'
+import roomModelUrl from '../../assets/models/room.glb?url'
+
 export default function Room() {
-  return (
-    <div className="room">
-        
-    </div>
-  )
+  const { scene } = useGLTF(roomModelUrl)
+
+  return <primitive object={scene} />
 }
+
+useGLTF.preload(roomModelUrl)
